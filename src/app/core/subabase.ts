88ac4492c0cd50bff26c3397eedc10444f722e9a
HASSAN <hassanalaa6060@gmail.com>
2026-03-10 03:47:29 +0200

    first commit

--- src/app/core/subabase.ts
+++ src/app/core/subabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import { environment } from '../enviroments/environment';
 
 export const supabase = createClient(
-  process.env['SUPABASE_URL']!,
-  process.env['SUPABASE_KEY']!  
+  environment.supabaseUrl,
+  environment.supabaseKey
 );
